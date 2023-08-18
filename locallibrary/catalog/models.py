7f74d19401415bdd82aeb99a07301a776eb152d1
Liu <doan.quang.luu@tpiv.sun-asterisk.com>
2023-08-18 18:16:38 +0700

    Django Tutorial Part 4: Django admin site

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
+
 # Create your models here.
 
 
@@ -37,6 +38,11 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    
+    display_genre.short_description = 'Genre'
 
 
 class BookInstance(models.Model):
